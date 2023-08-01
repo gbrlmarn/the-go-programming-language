875e469ab263822fab5213b15cbd7cabc7276249
Gabriel Marin <gbrlmarn@gmail.com>
2023-08-01 20:06:53 +0300

    ex3.10

---
+++ ch3/ex3.10/main.go
@@ -0,0 +1,27 @@
+// Exercie 3.10: Write a non-recursive version of comma, using bytes.Buffer instead of string concatenation
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+    fmt.Println(comma(os.Args[1]))
+}
+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+    var b bytes.Buffer
+    pre := len(s) % 3 
+
+    if pre != 0 {
+        b.WriteString(s[0:pre])
+    }
+    for i := pre; i < len(s); i += 3 {
+        b.WriteByte(',')
+        b.WriteString(s[i : i+3])
+    }
+    return b.String()
+}
---
+++ ch3/examples/comma/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkComma(b *testing.B) {
+    for i := 0; i < 99999; i++ {
+        comma(fmt.Sprint(i))
+    }
+}
