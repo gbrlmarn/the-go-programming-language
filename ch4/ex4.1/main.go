cd9c9635b3014ae0ac02fae3a0e7070122fba2d5
Gabriel Marin <gbrlmarn@proton.me>
2023-08-03 20:41:31 +0300

    ch4 ex4.1

---
+++ ch4/ex4.1/main.go
@@ -0,0 +1,56 @@
+// Exercise 4.1: Write a function that counts the number of bits that are different in two SHA256 hashes. (See PopCount from Section 2.6.2.)
+package main
+
+import (
+	"crypto/sha256"
+	"os"
+    "fmt"
+)
+
+var pc [256]byte
+
+func init() {
+    for i := range pc {
+        pc[i] = pc[i/2] + byte(i&1)
+    }
+}
+
+func main() {
+    var c1, c2 [32]byte 
+    if len(os.Args) == 3 {
+        c1 = sha256.Sum256([]byte(os.Args[1])) 
+        c2 = sha256.Sum256([]byte(os.Args[2]))
+    } else {
+        c1 = sha256.Sum256([]byte("x")) 
+        c2 = sha256.Sum256([]byte("X"))
+    }
+    fmt.Printf("Number of different bits: %d\n", bitDiff1(c1, c2))
+    fmt.Printf("Number of different bits: %d\n", bitDiff2(c1, c2))
+}
+
+func bitDiff1(c1, c2 [sha256.Size]byte) int {
+    var c int
+    for i := 0; i < sha256.Size; i++ {
+        for j := 0; j < 8; j++ {
+            if ((c1[i] >> j) & 1) != ((c2[i] >> j) & 1) {
+                c++
+            }
+        }
+    }
+    return c
+}
+func bitDiff2(c1, c2 [sha256.Size]byte) int {
+    var c int
+    for i := 0; i < sha256.Size; i++ {
+        c += int(pc[c1[i]^c2[i]])
+    }
+    return c
+}
+// goos: darwin
+// goarch: arm64
+// pkg: the-go-programming-language/ch4/ex4.1
+// BenchmarkBitDif1-8   	1000000000	         0.2140 ns/op
+// BenchmarkBitDif2-8   	1000000000	         0.1061 ns/op
+// PASS
+// ok  	the-go-programming-language/ch4/ex4.1	4.669s
+// The second one is better :D
---
+++ ch4/ex4.1/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"crypto/sha256"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+const (
+    lstr = 10
+    nstrs = 1000000
+)
+
+var tstr1 [nstrs]string
+var tstr2 [nstrs]string
+
+func init() {
+    rand.Seed(time.Now().UnixNano())
+    for i := 0; i < len(tstr1); i++ {
+        tstr1[i] = randS(lstr)
+    }
+    for i := 0; i < len(tstr2); i++ {
+        tstr2[i] = randS(lstr)
+    }
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randS(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
+}
+
+func BenchmarkBitDif1(b *testing.B) {
+    for i := 0; i < nstrs; i++ {
+        c1 := sha256.Sum256([]byte(tstr1[i]))
+        c2 := sha256.Sum256([]byte(tstr2[i]))
+        bitDiff1(c1, c2)
+    }    
+}
+
+
+func BenchmarkBitDif2(b *testing.B) {
+    for i := 0; i < nstrs; i++ {
+        c1 := sha256.Sum256([]byte(tstr1[i]))
+        c2 := sha256.Sum256([]byte(tstr2[i]))
+        bitDiff2(c1, c2)
+    }    
+}
