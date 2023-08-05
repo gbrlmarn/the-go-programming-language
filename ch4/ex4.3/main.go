3b95e7bc85bc2703babcabd3090fdcf3eca5537b
Gabriel Marin <gbrlmarn@gmail.com>
2023-08-05 12:57:52 +0300

    4.3

---
+++ ch4/ex4.3/main.go
@@ -0,0 +1,23 @@
+// Exercise 4.3: Rewrite reverse to use an array pointer instead of a slice
+package main
+
+import "fmt"
+
+func main() {
+    arr := [5]int{1, 2, 3, 4, 5}
+    fmt.Println(arr)
+    reverse(&arr)
+    fmt.Println(arr)
+}
+
+func reverse(sp *[5]int) {
+    for i, j := 0, len(sp)-1; i < j; i, j = i+1, j-1 {
+        sp[i], sp[j] = sp[j], sp[i]
+    }
+}
+func reverse2(sp *[5]int) {
+    for i, j := 0, len(sp)-1; i < j; i, j = i+1, j-1 {
+        // C style deref...
+        (*sp)[i], (*sp)[j] = (*sp)[j], (*sp)[i]
+    }
+}
---
+++ ch4/examples/rev/main.go
@@ -0,0 +1,17 @@
+// reverse reverses a slice of ints in place.
+package main
+
+import "fmt"
+
+func main() {
+    arr := [...]int{1, 2, 3, 4, 5}
+    fmt.Println(arr)
+    reverse(arr[:])
+    fmt.Println(arr)
+}
+
+func reverse(s []int) {
+    for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+        s[i], s[j] = s[j], s[i]
+    }
+}
