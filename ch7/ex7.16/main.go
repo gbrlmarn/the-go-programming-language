b39720e91e725805efad5eb8dc238cc84844b896
Gabriel Marin <gbrlmarn@proton.me>
2023-11-08 18:14:00 +0200

    ex7.16

---
+++ ch7/ex7.16/main.go
@@ -0,0 +1,33 @@
+// Exercise 7.16: Write a web-based calculator program.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"the-go-programming-language/ch7/examples/eval"
+)
+
+func main() {
+	http.HandleFunc("/", calculate)
+	log.Fatal(http.ListenAndServe("localhost:3000", nil))
+}
+
+func calculate(w http.ResponseWriter, r *http.Request) {
+	s := r.FormValue("eval")
+	expr, err := eval.Parse(s)
+	if err != nil {
+		http.Error(w, "bad expr: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	env := make(eval.Env)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound) // 404
+	}
+	vars := make(map[eval.Var]bool)
+	if err := expr.Check(vars); err != nil {
+		w.WriteHeader(http.StatusNotFound) // 404
+	}
+	fmt.Fprintf(w, "%v\n", expr.Eval(env))
+}
