d6229c5b4f083f9c25073de8d9d4be0ddd0955d0
Marin Gabriel <marin.gabriel@protonmail.com>
2024-01-25 19:54:36 +0200

    ex12.1

---
+++ ch12/examples/format/format_test.go
@@ -0,0 +1,23 @@
+package format_test
+
+import (
+	"fmt"
+	"gopl/ch12/examples/format"
+	"testing"
+	"time"
+)
+
+func Test(t *testing.T) {
+    var x int64 = 1
+    var d time.Duration = 1 * time.Nanosecond
+    fmt.Println(format.Any(x)) 
+    fmt.Println(format.Any(d))
+    fmt.Println(format.Any([]int64{x}))
+    fmt.Println(format.Any([]time.Duration{d}))
+}
+// 1
+// 1
+// []int64 0xc000012180
+// []time.Duration 0xc000012188
+// PASS
+// ok  	gopl/ch12/examples/format	0.002s
