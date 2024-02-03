02bf655c7061a04f272a12ee1f2fad431b422b8e
Marin Gabriel <marin.gabriel@protonmail.com>
2024-02-03 12:40:52 +0200

    ex12.3

---
+++ ch12/ex12.3/sexpr_test.go
@@ -0,0 +1,42 @@
+package sexpr
+
+import "testing"
+
+func Test(t *testing.T) {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Color           bool
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Color:    false,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+    // Encoding 
+    data, err := Marshal(strangelove)
+    if err != nil {
+        t.Fatalf("Marshal failed: %v", err)
+    }
+    t.Logf("Marshal() = %s\n", data)
+}
