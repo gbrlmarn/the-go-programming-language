39d4ead41e390e8be0f098fb456b83b7a8fcb289
Marin Gabriel <marin.gabriel@protonmail.com>
2024-02-07 21:01:33 +0200

    ex12.7

---
+++ ch12/ex12.7/sexpr_test.go
@@ -0,0 +1,54 @@
+package sexpr
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Color           bool
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Color:    false,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+    // Encoding 
+    data, err := Marshal(strangelove)
+    if err != nil {
+        t.Fatalf("Marshal failed: %v", err)
+    }
+    t.Logf("Marshal() = %s\n", data)
+
+    // Encoder
+    buf := new(bytes.Buffer)
+    e := NewEncoder(buf)
+    err = e.Encode(strangelove)
+    if err != nil {
+        t.Fatalf("e.Encode failed: %v", err)
+    }
+    t.Logf("e.Encode() = %s\n", buf)
+}
