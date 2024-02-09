b58fb6ba87794065d313d2bb513e27b259ffe521
Marin Gabriel <marin.gabriel@protonmail.com>
2024-02-09 21:34:26 +0200

    ex12.8

---
+++ ch12/ex12.8/decode.go
@@ -0,0 +1,141 @@
+// Exercise 12.8: The sexpr.Unmarshal function, like json.Unmarshal, requires the complete input in a byte slice before it can begin decoding. Define a sexpr.Decoder type that, like json.Decoder, allows a sequence of values to be decoded from an io.Reader. Change sexpr.Unmarshal to use this new type.
+package sexpr
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+	"text/scanner"
+)
+
+type lexer struct {
+	scan  scanner.Scanner
+	token rune // the current rune
+}
+
+func (lex *lexer) next()        { lex.token = lex.scan.Scan() }
+func (lex *lexer) text() string { return lex.scan.TokenText() }
+
+func (lex *lexer) consume(want rune) {
+	if lex.token != want { // NOTE: Not an example of good error handling.
+		panic(fmt.Sprintf("got %q, want %q", lex.text(), want))
+	}
+	lex.next()
+}
+
+func read(lex *lexer, v reflect.Value) {
+	switch lex.token {
+	case scanner.Ident:
+		// The only valid identifiers are
+		// "nil" and struct field names.
+		if lex.text() == "nil" {
+			v.Set(reflect.Zero(v.Type()))
+			lex.next()
+			return
+		}
+	case scanner.String:
+		s, _ := strconv.Unquote(lex.text()) // NOTE: ignoring errors
+		v.SetString(s)
+		lex.next()
+		return
+	case scanner.Int:
+		i, _ := strconv.Atoi(lex.text()) // NOTE: ignoring errors
+		v.SetInt(int64(i))
+		lex.next()
+		return
+	case '(':
+		lex.next()
+		readList(lex, v)
+		lex.next() // consume ')'
+		return
+	}
+	panic(fmt.Sprintf("unexpected token %q", lex.text()))
+}
+
+func readList(lex *lexer, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Array: // (item ...)
+		for i := 0; !endList(lex); i++ {
+			read(lex, v.Index(i))
+		}
+
+	case reflect.Slice: // (item ...)
+		for !endList(lex) {
+			item := reflect.New(v.Type().Elem()).Elem()
+			read(lex, item)
+			v.Set(reflect.Append(v, item))
+		}
+
+	case reflect.Struct: // ((name value) ...)
+		for !endList(lex) {
+            lex.consume('(')
+            if lex.token != scanner.Ident {
+                panic(fmt.Sprintf("got token %q, want field name", lex.text()))
+            }
+            name := lex.text()
+            lex.next()
+            read(lex, v.FieldByName(name))
+            lex.consume(')')
+		}
+
+	case reflect.Map: // ((key value) ...)
+        v.Set(reflect.MakeMap(v.Type()))
+        for !endList(lex) {
+            lex.consume('(')
+            key := reflect.New(v.Type().Key()).Elem()
+            read(lex, key)
+            value := reflect.New(v.Type().Elem()).Elem()
+            read(lex, value)
+            v.SetMapIndex(key, value)
+            lex.consume(')')
+        }
+
+	default:
+		panic(fmt.Sprintf("cannot decode list into %v", v.Type()))
+	}
+}
+
+func endList(lex *lexer) bool {
+	switch lex.token {
+	case scanner.EOF:
+		panic("end of file")
+	case ')':
+		return true
+	}
+	return false
+}
+
+type Decoder struct {
+    lex *lexer
+}
+
+func NewDecoder(r io.Reader) *Decoder {
+    scan := scanner.Scanner{Mode: scanner.GoTokens}
+    scan.Init(r)
+    return &Decoder{&lexer{scan: scan}}
+}
+
+func (d *Decoder) Decode(v any) (err error) {
+    d.lex.next() // get the first token
+    defer func() {
+        // NOTE: this is not an exmaple of ideal error handling.
+        if x := recover(); x != nil {
+            err = fmt.Errorf("error at %s: %v", d.lex.scan.Position, x)
+        }
+    }()
+    read(d.lex, reflect.ValueOf(v).Elem())
+    return nil
+}
+
+func (d *Decoder) More() bool {
+    return d.lex.token != scanner.EOF
+}
+
+// Unmarshal parses S-expression data and populates the variable
+// whose address is in the non-nil pointer out.
+func Unmarshal(data []byte, out interface{}) (err error) {
+    d := NewDecoder(bytes.NewReader(data))
+    return d.Decode(out)
+}
